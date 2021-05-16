021b7275ccd7d7d6e99175844ed32bf8cd3bf4ee
lijealso <lijealso@gmail.com>
2021-05-16 12:36:28 +0100

    Update converter-preto-branco.py

--- Python Imaging Library/converter-preto-branco.py
+++ Python Imaging Library/converter-preto-branco.py
@@ -1,1 +1,6 @@
-
+# Converter Imagem para preto e branco
+from PIL import Image
+img = Image.open("lince.jpg")
+BlackAndWhite = img.convert("L")
+BlackAndWhite.save("pb_lince.jpg")
+BlackAndWhite.show()
