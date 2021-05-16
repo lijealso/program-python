b9eef8fd056cbe4bfc99e6f778de6a21c1f2c500
lijealso <lijealso@gmail.com>
2021-05-16 16:26:13 +0100

    adicionar comentários

--- Python Imaging Library/miniatura.py
+++ Python Imaging Library/miniatura.py
@@ -1,14 +1,18 @@
 import os, sys
 from PIL import Image
 
+# tamanho da miniatura
 size = (128, 128)
 
 for infile in sys.argv[1:]:
   outfile = os.path.splitext(infile)[0] + ".thumbnail"
+#  quando infile NÃO É IGUAL A outfile
   if infile != outfile:
     try:
+# abre a imagem   
       with Image.open(infile) as img:
         img.thumbnail(size)
+# grava a miniatura
         img.save(outfile, "JPEG")
     except OSError:
       print("não foi possível criar miniatura para", infile)
