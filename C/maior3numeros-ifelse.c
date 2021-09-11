5d1f747df24efe556810003ab6101f153e6713ae
lijealso <lijealso@gmail.com>
2021-09-11 20:16:26 +0100

    código

--- C/maior3numeros-ifelse.c
+++ C/maior3numeros-ifelse.c
@@ -1,1 +1,41 @@
+// Programa para encontrar o maior número entre 3 números, usando if else
+#include <stdio.h>
 
+void main()
+{
+    int x , y , z ;                             // declaração de variáveis
+
+    printf("Introduza o primeiro número :");
+    scanf("%d",&x);                             // input do primeiro número
+
+    printf("Introduza o segundo número :");
+    scanf("%d",&y);                             // input do segundo número
+
+    printf("Introduza o terceiro número :");
+    scanf("%d",&z);                             // input do terceiro número
+
+    if (x > y)                                  // Verificar o maior entre o número 1 e número 2
+    {
+        if (x > z)                              // se o primeiro é maior, verificar com o terceiro
+        {
+            printf("O número %d é o maior.",x);     // se as condições acima forem verdade, o número 1 é o maior, else, terceiro ? é maior
+        }
+        else
+        {
+            printf("O número %d é o maior.",z);
+        }
+    }
+    else                                            // se o número 2 é maior, comparar com o terceiro
+    {
+        if (y > z)                                  // se verdade, o número 2 é o maior, else, o número 2 é o maior
+        {
+            printf("O número %d é o maior.",y);
+        }
+        else
+        {
+            printf("O número %d é o maior.",z);
+        }
+    }
+
+
+}
