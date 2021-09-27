4430adb6dd68dafab2b64a86af6d0c9ce99cc67b
lijealso <lijealso@gmail.com>
2021-09-27 01:32:48 +0100

    step 4, pressionar q para sair

--- C/editor/kilo.c
+++ C/editor/kilo.c
@@ -2,6 +2,6 @@
 
 int main(){
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1); // lê 1 byte do standard input para a variável c
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); // lê 1 byte do standard input para a variável c, quando lê a letra q, sai do programa
 return 0;
 }
